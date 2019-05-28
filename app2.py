522ef1ffee722d402af9bb12350c04a444e8bea6
Megha Agarwal <magarwa0@chicagobooth.edu>
2019-05-28 16:21:05 -0500

    first commit to call allocation optimization project'

---
+++ app2.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 13:55:25 2019
+@author: Aveedibya Dey
+"""
+import numpy as np
+import datetime as dt
+import pandas as pd
+import json
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output, State
+import dash_table
+import statistics as st
+
+#Internal package with functions and classes
+import call_gen_demo as cgd
+from call_generator_distribution import agentAggMetrics, overallMetrics
+
+app = dash.Dash()
+app.title = 'Blended Call Optimizer'
+#server = app.server
+
+#-------------------------------
+#App Layout
+app.layout = html.Div([
+                html.Div([html.H5("Step 1: Define Call Distribution: ")], className=''),
+
+                html.Hr(),
+
+                html.Div([html.H5("Step 2: Define Cost Parameters for Cost Allocation Function: ")], className=''),
+
+                #Start: Input Cost Parameters
+                html.Div(html.Div([html.Label('Select your allocation method:'),
+                                    dcc.Dropdown(id='allocation-method', options=[
+                                            {'label': 'Random Allocation', 'value': 0},
+                                            {'label': 'Systematic Cost Based Allocation', 'value': 1}], value=1)], className='col'), className='row'),
+
+                html.Div([html.Div(html.Div(html.Div([
+                            html.H5("Agent Idle Cost", className='card-title'),
+                            html.P("Cost associated with idle time will be accounted using this factor.", className='card-text'),
+                            html.Div([html.Label('Factor = ',),
+                                        dcc.Input(id='factor-1', type='number', step=1, placeholder='7/30', className='form-control')], className='form-group'),
+                            html.Div([html.Label('Weight = ',),
+                                        dcc.Input(id='weight-factor-1', type='number', step=0.1, placeholder='0.5', className='form-control')], className='form-group')
+                        ], className='card-body'), className='card shadow-sm'), className='col-4'),
+
+                        html.Div(html.Div(html.Div([
+                            html.H5("Switching Cost", className='card-title'),
+                            html.P("Cost associated with switching between various contact methods will be accounted by this factor.", className='card-text'),
+                            html.Div([html.Label('Factor = ',),
+                                        dcc.Input(id='factor-2', type='number', step=1, placeholder='7/30', className='form-control')], className='form-group'),
+                            html.Div([html.Label('Weight = ',),
+                                        dcc.Input(id='weight-factor-2', type='number', step=0.1, placeholder='0.5', className='form-control')], className='form-group')
+                        ], className='card-body'), className='card shadow-sm'), className='col-4'),
+
+                        html.Div(html.Div(html.Div([
+                            html.H5("Distribution Skewness Cost", className='card-title'),
+                            html.P("Cost associated with skewed distribution of calls will be accounted by this factor.", className='card-text'),
+                            html.Div([html.Label('Factor = ',),
+                                        dcc.Input(id='factor-3', type='number', step=1, placeholder='7/30', className='form-control')], className='form-group'),
+                            html.Div([html.Label('Weight = ',),
+                                        dcc.Input(id='weight-factor-3', type='number', step=0.1, placeholder='0.5', className='form-control')], className='form-group')
+                        ], className='card-body'), className='card shadow-sm'), className='col-4'),
+                ], className='row mt-4'),
+                #End: Input Cost Paramenters
+
+                html.Hr(),
+
+                html.Div([html.Button("Run Call Allocation Simulation", id='run-simulation', className='btn btn-outline-info btn-block')]),
+
+                html.Hr(),
+
+                dcc.Loading(children=[
+                        
+                html.Div([html.H5(id='result-header', children="Results: Simulation based on cost allocation is shown below: ")], className='', style={'display': 'none'}),
+
+                html.Div(id='intermediate-values', style={'display': 'none'}),
+                html.Div(id='allocation-results'),
+
+                html.H5(id='result-header-1', children='Overall Metrics:', className='my-3', style={'display': 'none'}),
+
+                html.Div(id='overall-metrics', children=[], className='row'),
+
+                html.H5(id='result-header-2', children='Average Agent Metrics:', className='my-3', style={'display': 'none'}),
+
+                html.Div(id='agent-metrics', children=[], className='row'),
+
+                html.Div([html.Div(dcc.Graph(id='agent-idle-times'), className='col-6')
+                         ], className='row'),
+
+
+                html.Br(), html.Br(),
+                
+                dash_table.DataTable(id='table')], type='cube'),
+
+            ], className='container')
+
+
+#-------------------------------
+
+def metrics_display(agent_metrics):
+    return [html.Div([html.H6('Number of Agents:'), html.P(str(len(agent_metrics)), className='display-4')], className='col mx-1 text-center alert alert-info'),
+         html.Div([html.H6('Call Distribution (Inbound):'),
+                    html.P("{0:.1%}".format(agent_metrics['inbound'].mean()), className='display-4'),
+                    html.Br(),
+                    html.H6('Std. Deviation:'),
+                    html.P("{0:.1%}".format(st.pstdev(agent_metrics['inbound'])), className='display-4')], className='col mx-1 text-center alert alert-info'),
+         html.Div([html.H6('Call Distribution (Outbound):'),
+                    html.P("{0:.1%}".format(agent_metrics['outbound'].mean()), className='display-4'),
+                    html.Br(),
+                    html.H6('Std. Deviation:'),
+                    html.P("{0:.1%}".format(st.pstdev(agent_metrics['outbound'])), className='display-4')], className='col mx-1 text-center alert alert-info'),
+         html.Div([html.H6('Number of Call Switches:'),
+                    html.P(str(round(agent_metrics['number_of_switches'].mean(),1)), className='display-4'),
+                    html.Br(),
+                    html.H6('Std. Deviation:'),
+                    html.P(str(round(st.pstdev(agent_metrics['number_of_switches']),1)), className='display-4')], className='col mx-1 text-center alert alert-info')]
+
+def overall_metrics_display(overall_metrics):
+    return [html.Div([html.H6('Total Call Count:'), html.P(overall_metrics['total_call_count'], className='display-4')], className='col mx-1 text-center alert alert-secondary'),
+             html.Div([html.H6('Call Distribution (Inbound):'), html.P("{0:.1%}".format(overall_metrics['call_count_by_type'][0]/sum(overall_metrics['call_count_by_type'])), className='display-4')], className='col mx-1 text-center alert alert-secondary'),
+             html.Div([html.H6('Call Distribution (Outbound):'), html.P("{0:.1%}".format(overall_metrics['call_count_by_type'][1]/sum(overall_metrics['call_count_by_type'])), className='display-4')], className='col mx-1 text-center alert alert-secondary'),
+             html.Div([html.H6('Avg. Wait Time (sec):'), html.P(str(round(overall_metrics['avg_call_wait'],1)), className='display-4')], className='col mx-1 text-center alert alert-secondary')
+             ]
+
+#-------------------------------
+#App Callback Functions
+
+@app.callback([Output('table', 'data'), Output('table', 'columns'),
+                Output('agent-metrics', 'children'),
+                Output('overall-metrics', 'children'),
+                Output('result-header', 'style'),
+                Output('result-header-1', 'style'),
+                Output('result-header-2', 'style'),
+                Output('agent-idle-times', 'figure')],
+              [Input('run-simulation', 'n_clicks'),
+               Input('allocation-method', 'value')])
+
+def calculate_metrics(n_clicks, allocation_method):
+    
+    if n_clicks is not None:
+        intvl_avg_calls = list(range(0,24,1)) + list(range(24,0,-1))
+        intvl_call_count = [np.random.poisson(x) for x in intvl_avg_calls]
+        max_intvl_calls = 10
+        intvl_avg_calls = [x*max_intvl_calls/max(intvl_avg_calls) for x in intvl_avg_calls]
+        intvl_st_time_day = [(dt.datetime(2018,1,1,0,0,0) + dt.timedelta(minutes= +30*x))  for x in range(len(intvl_avg_calls))]
+        intvl_st_time = [dt.time(x.hour, x.minute, x.second) for x in intvl_st_time_day]
+        intvl_call_count = [np.random.poisson(x) for x in intvl_avg_calls]
+        intvl_call_count = [np.random.poisson(x) for x in intvl_avg_calls]
+        aht_range = [300, 400]
+        agent_count = 3
+        
+        call_tbl = cgd.call_table(intvl_st_time, intvl_call_count, aht_range)
+        agent_tbl = cgd.agent_table(int(agent_count), call_tbl, use_cost_calculation=allocation_method)
+        agent_metrics = agentAggMetrics(agent_tbl)
+        overall_metrics = overallMetrics(agent_tbl)
+
+        agent_metrics_to_display = metrics_display(agent_metrics)
+        overall_metrics_to_display = overall_metrics_display(overall_metrics)
+        
+        #plot agent metrics
+        idle_traces = []
+        
+        idle_traces.append(go.Bar(
+                x=agent_metrics['agent_index'],
+                y=agent_metrics['idle_time'],
+                text=['Agent ' + str(agent_metrics['agent_index'][x]) + ' idles for: ' + "{0:.1%}".format(agent_metrics['idle_time'][x]) for x in agent_metrics.index.tolist()],
+                hoverinfo='text',
+                marker=dict(
+                        color='rgb(58,200,225)',
+                        line=dict(
+                                color='rgb(8,48,107)',
+                                width=1.5),
+                                ),
+                name="Agent Idle Times (% of total time)",
+                opacity=0.6
+                ))
+
+        return agent_metrics.to_dict('records'), [{"name": i, "id": i} for i in agent_metrics.columns],\
+        agent_metrics_to_display, overall_metrics_to_display, {'display': ''}, {'display': ''}, {'display': ''},\
+        {'data': idle_traces, 
+         'layout': go.Layout(
+                 title='Agent Idle Time (in percentage of overall time)',
+                 xaxis={'zeroline': False, 'showgrid': False, 'showticklabels': False},
+                 yaxis={'zeroline': False, 'showgrid': False, 'range': [0.9*min(agent_metrics['idle_time']), 1.1*max(agent_metrics['idle_time'])], 'tickformat': ",.1%"}
+                 )
+         }
+    else:
+        return '','', '', '', {'display': 'none'}, {'display': 'none'}, {'display': 'none'}, {}
+
+
+#def charting():
+#    trace1 = go.Bar(
+#    x=x,
+#    y=y,
+#    text=y,
+#    textposition = 'auto',
+#    marker=dict(
+#        color='rgb(158,202,225)',
+#        line=dict(
+#            color='rgb(8,48,107)',
+#            width=1.5),
+#        ),
+#    opacity=0.6
+#    )
+#    
+#    trace2 = go.Bar(
+#        x=x,
+#        y=y2,
+#        text=y2,
+#        textposition = 'auto',
+#        marker=dict(
+#            color='rgb(58,200,225)',
+#            line=dict(
+#                color='rgb(8,48,107)',
+#                width=1.5),
+#            ),
+#        opacity=0.6
+#        )
+#
+#    data = [trace1,trace2]
+
+#-------------------------------
+#External CSS Links
+external_css = ["https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"]
+
+for css in external_css:
+    app.css.append_css({"external_url": css})
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ call_generator_distribution.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 21 23:38:36 2019
+
+@author: avee
+"""
+import numpy as np
+import random
+import pandas as pd
+import datetime as dt
+
+import call_gen_demo as cgd
+
+class dynamicCall:
+    """
+    """
+    #call_type = 'inbound'
+    
+    def __init__(self, aht_range, arrival_intvl, call_proportions=[0.3, 0.7], call_types=['inbound', 'outbound']):
+        self.aht_actual = random.randint(aht_range[0], aht_range[1])
+        self.call_start_time = dt.time(arrival_intvl.hour, arrival_intvl.minute + random.randint(1,29))
+        
+        curr_random_selector = random.randint(0,100)/100
+        #print(curr_random_selector)
+        call_proportions_compare = 0
+        for i in range(len(call_proportions)):
+            call_proportions_compare += call_proportions[i]
+            #print(call_proportions_compare)
+            if curr_random_selector <= call_proportions_compare:
+                self.call_type = call_types[i]
+                break
+
+
+def calculateAgentCosts(agent_costs, call_types, next_call, agent_tbl, call_switch_agent_time=7.00, call_bp_time=60.00):
+    """
+    """
+    next_call= next_call.reset_index()
+    call_distribution_actual, call_distribution_ratios = currentParameters(call_types, agent_tbl)
+    
+    for agent_index in agent_costs['agent_index'].drop_duplicates().tolist():
+        call_distribution_list = []
+        for call_type_index in range(len(call_types)):
+            call_distribution_list.append(len(agent_tbl[(agent_tbl['agent_index']==agent_index) \
+                                                        & (agent_tbl['call_type']==call_types[call_type_index])]))
+        
+        agent_costs['call_skewness'][agent_index] = call_distribution_list
+        agent_costs['idle_time'][agent_index] = 1 - agent_tbl[agent_tbl['agent_index']==agent_index]['call_aht'].sum()/86400
+        #agent_costs
+        
+        next_call_type_index = call_types.index(next_call['call_type'][0])
+        
+        
+        if sum(agent_costs['call_skewness'][agent_index]) > 0:
+            distribution_cost_factor = call_distribution_ratios[next_call_type_index] - \
+            agent_costs['call_skewness'][agent_index][next_call_type_index]/sum(agent_costs['call_skewness'][agent_index])
+        else:
+            distribution_cost_factor = 0
+            
+        #print(distribution_cost_factor)
+        
+        idleness_cost_factor = agent_costs['idle_time'][agent_index]
+        #print(idleness_cost_factor)
+        switch_cost_factor = 0
+        
+        if agent_costs['last_call_type'].notnull()[agent_index]:
+            if call_types.find(agent_costs['last_call_type'][agent_index]) != next_call_type_index:
+                switch_cost_factor = call_switch_agent_time/call_bp_time
+        
+        #print(switch_cost_factor)
+        agent_costs['assignment_cost'][agent_index] = distribution_cost_factor + idleness_cost_factor + switch_cost_factor
+    
+    #print("---->>--Agent Costs calculated:\n", agent_costs)
+    return agent_costs
+
+
+def currentParameters(call_types, agent_tbl):
+    """
+    """
+    call_distribution_actual = []
+    call_distribution_ratios = []
+    
+    for call_type_index in range(len(call_types)):
+        call_distribution_actual.append(len(agent_tbl[(agent_tbl['call_type']==call_types[call_type_index])]))
+     
+    for call_type_index in range(len(call_types)):
+        if sum(call_distribution_actual) > 0:
+            call_distribution_ratios.append(call_distribution_actual[call_type_index]/sum(call_distribution_actual))
+        else:
+            call_distribution_ratios.append(0)
+     
+    return call_distribution_actual, call_distribution_ratios
+
+
+def pickLeastCostlyAgent(agent_status, call_types, next_call, 
+                         agent_tbl, call_switch_agent_time=7.00, call_bp_time=60.00):
+    """
+    """
+    agent_costs = pd.DataFrame(columns=['agent_index', 'call_skewness', 'idle_time', \
+                                    'last_call_type', 'agent_status', 'assignment_cost'])  
+    
+    agent_costs['agent_index'] = range(len(agent_status))
+    agent_costs['agent_status'] = agent_status
+    
+    agent_costs = calculateAgentCosts(agent_costs, call_types, next_call, agent_tbl, call_switch_agent_time, call_bp_time)
+    if len(agent_costs[agent_costs['agent_status']==1]) == 0:
+        #All agents currently busy
+        agent_avail_list = cgd.agentNextAvail(agent_status, agent_table_df=agent_tbl)
+        pickedAgent = agent_avail_list.index(min(agent_avail_list))
+    else:
+        #print("Here is the length:", len(agent_costs[agent_costs['agent_status']==1]))
+        pickfromAgents = agent_costs[agent_costs['agent_status']==1]['agent_index'].tolist()
+        #print("Pick from these agents: ", pickfromAgents)
+        pickedAgent = pickfromAgents[agent_costs[agent_costs['agent_status']==1]['assignment_cost'].tolist().index(max(agent_costs[agent_costs['agent_status']==1]['assignment_cost']))]
+    
+    #print("------->>----")
+    #print(agent_costs)
+    return pickedAgent
+
+
+def agentAggMetrics(agent_tbl, call_types=['inbound', 'outbound']):
+    
+    agent_list = agent_tbl['agent_index'].drop_duplicates().tolist()
+    col_list = ['agent_index', 'call_aht', 'idle_time', 'number_of_switches']
+    for i in call_types:
+        col_list.append(i)
+    agent_metrics = pd.DataFrame(columns=col_list)
+    agent_metrics['agent_index'] = agent_list
+    agent_metrics_call_aht = []
+    number_of_switches = []
+    
+    for agent_index in agent_list:
+        #print(agent_metrics)
+        agent_metrics_call_aht.append(agent_tbl[agent_tbl['agent_index']==agent_index]['call_aht'].sum())
+        for call_type_i in call_types:
+            agent_metrics.loc[agent_metrics['agent_index']==agent_index, call_type_i] = agent_tbl[(agent_tbl['agent_index']==agent_index) & (agent_tbl['call_type']==call_type_i)]['call_type'].count()/agent_tbl[agent_tbl['agent_index']==agent_index]['call_type'].count()
+        
+        switch_count_tmp = 0
+        for call_index in agent_tbl[agent_tbl['agent_index']==agent_index].index.tolist()[0:-1]:
+            if agent_tbl['call_type'][call_index] != agent_tbl['call_type'][call_index+1]:
+                switch_count_tmp += 1
+        number_of_switches.append(switch_count_tmp)
+        #print(number_of_switches)
+        
+    
+    agent_metrics['call_aht'] = agent_metrics_call_aht
+    agent_metrics['idle_time'] = 1 - agent_metrics['call_aht']/86400
+    agent_metrics['number_of_switches'] = number_of_switches
+    
+    
+    
+    return agent_metrics
+
+def overallMetrics(agent_tbl, call_types=['inbound', 'outbound']):
+    """
+    """
+    total_call_count = len(agent_tbl)
+    avg_call_wait = agent_tbl['call_wait_time_elapsed'].mean()
+    call_count_by_type = []
+    for call_type in call_types:
+        call_count_by_type.append(len(agent_tbl[agent_tbl['call_type']==call_type]))
+        
+    return {'total_call_count': total_call_count,
+            'avg_call_wait': avg_call_wait,
+            'call_count_by_type': call_count_by_type}
+
+if __name__ == '__main__':
+    intvl_avg_calls = list(range(0,24,1)) + list(range(24,0,-1))
+    intvl_call_count = [np.random.poisson(x) for x in intvl_avg_calls]
+    max_intvl_calls = 10
+    intvl_avg_calls = [x*max_intvl_calls/max(intvl_avg_calls) for x in intvl_avg_calls]
+    intvl_st_time_day = [(dt.datetime(2018,1,1,0,0,0) + dt.timedelta(minutes= +30*x))  for x in range(len(intvl_avg_calls))]
+    intvl_st_time = [dt.time(x.hour, x.minute, x.second) for x in intvl_st_time_day]
+    intvl_call_count = [np.random.poisson(x) for x in intvl_avg_calls]
+    intvl_call_count = [np.random.poisson(x) for x in intvl_avg_calls]
+    aht_range = [300, 400]
+    agent_count = 3
+    call_tbl = cgd.call_table(intvl_st_time, intvl_call_count, aht_range)
+    agent_tbl = cgd.agent_table(int(agent_count), call_tbl, use_cost_calculation=0)
+ 
+    #call_types=['inbound', 'outbound']
+    #cgd.updateAgentStatus(call_tbl['call_start_time'][10], [1,1,1], agent_tbl)
+    #agentPicked = pickLeastCostlyAgent([1,1,1], call_types, call_tbl[9:10], agent_tbl)
+    #print("Agent picked is: ", agentPicked)
+ 
+ 
